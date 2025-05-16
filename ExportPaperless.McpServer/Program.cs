05a2c6a006b496f2def0f56d14324fb69434fbac
Sandro Lange <info@thesmartno.de>
2025-05-16 17:21:18 +0200

    feat: first McpServer implementation refactor: hugely refactor services, configuration and controller to offer more flexibilities

---
+++ ExportPaperless.McpServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using ExportPaperless.Domain.Services;
+using ExportPaperless.McpServer.Services;
+using ExportPaperless.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose() // Capture all log levels
+    .WriteTo.File(Path.Combine("/tmp", "logs", "ExportPaperless_McpServer_.log"),
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .WriteTo.Debug()
+    .WriteTo.Console(standardErrorFromLevel: Serilog.Events.LogEventLevel.Verbose)
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting server...");
+
+    var builder = Host.CreateApplicationBuilder(args);
+    builder.Services.AddSerilog();
+    builder.Services.AddServices();
+    builder.Services.AddTransient<IMcpConfigurationService, McpConfigurationService>();
+    builder.Services.AddMcpServer()
+        .WithStdioServerTransport()
+        .WithToolsFromAssembly();
+
+    var app = builder.Build();
+
+    await app.RunAsync();
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
---
+++ ExportPaperless.Storage/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ExportPaperless.Domain.Services;
+using ExportPaperless.Storage.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExportPaperless.Storage;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddStorage(this IServiceCollection services)
+    {
+        services.AddTransient<IStorageConfigurationService, StorageConfigurationService>();
+        services.AddTransient<IStorageService, StorageService>();
+    }
+}
